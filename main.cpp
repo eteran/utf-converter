f3b12f1378661ac971a747da87a7ddc0407c231e
Evan Teran <evan.teran@gmail.com>
2016-03-10 15:55:36 -0500

    improving implementation, prepping for more features

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 #include <fstream>
 #include <cstdint>
 #include <vector>
+#include <iterator>
 #include <exception>
 
-enum UtfEncoding {
+namespace utf {
+
+enum Encoding {
 	UTF8,
 	UTF16_BE,
 	UTF16_LE,
@@ -32,90 +35,82 @@ class invalid_unicode_character : public std::exception {
 	file.seekg (0, std::ios_base::beg);
 #endif
 
-std::istream &readUtf16Value(std::istream &is, uint32_t *value) {
+std::istream &read_codepoint(std::istream &is, Encoding encoding, uint32_t *codepoint) {
 
-	uint16_t w1;
-	uint16_t w2;
-	if(!is.read(reinterpret_cast<char *>(&w1), sizeof(w1))) {
-		return is;
-	}
-	
-	// part of a surrogate pair
-	if((w1 & 0xfc00) == 0xd800) {
-		if(!is.read(reinterpret_cast<char *>(&w2), sizeof(w2))) {
-			return is;
-		}	
-	
-	}
-	
-	uint32_t cp;
-	if((w1 & 0xfc00) == 0xd800) {
-		if((w2 & 0xfc00) == 0xdc00) {
-			cp = 0x10000 + (((static_cast<uint32_t>(w1) & 0x3ff) << 10) | (w2 & 0x3ff));
-		} else {
-			throw invalid_unicode_character();
+
+	switch(encoding) {
+	case UTF16_LE:
+		{
+			uint16_t w1;
+			uint16_t w2;
+			if(!is.read(reinterpret_cast<char *>(&w1), sizeof(w1))) {
+				return is;
+			}
+
+			// part of a surrogate pair
+			if((w1 & 0xfc00) == 0xd800) {
+				if(!is.read(reinterpret_cast<char *>(&w2), sizeof(w2))) {
+					throw invalid_unicode_character();
+				}	
+
+			}
+
+			uint32_t cp;
+			if((w1 & 0xfc00) == 0xd800) {
+				if((w2 & 0xfc00) == 0xdc00) {
+					cp = 0x10000 + (((static_cast<uint32_t>(w1) & 0x3ff) << 10) | (w2 & 0x3ff));
+				} else {
+					throw invalid_unicode_character();
+				}
+			} else {
+				cp = w1;
+			}
+
+			*codepoint = cp;
 		}
-	} else {
-		cp = w1;
+		break;
+	default:
+		abort();
 	}
-	
-	*value = cp;
 
 	return is;
 }
 
-std::vector<uint8_t> codepointToUtf8(uint32_t cp) {
+template <class Out>
+void write_codepoint(uint32_t cp, Encoding encoding, Out &&out) {
 
-	std::vector<uint8_t> utf8;
-	auto out = std::back_inserter(utf8);
-
-	if(cp < 0x80) {
-		*out++ = static_cast<uint8_t>(cp);
-	} else if(cp < 0x0800) {
-		*out++ = static_cast<uint8_t>(0xc0 | ((cp >> 6) & 0x1f));
-		*out++ = static_cast<uint8_t>(0x80 | (cp & 0x3f));
-	} else if(cp < 0x10000) {
-		*out++ = static_cast<uint8_t>(0xe0 | ((cp >> 12) & 0x0f));
-		*out++ = static_cast<uint8_t>(0x80 | ((cp >> 6) & 0x3f));
-		*out++ = static_cast<uint8_t>(0x80 | (cp & 0x3f));
-	} else if(cp < 0x1fffff) {
-		*out++ = static_cast<uint8_t>(0xf0 | ((cp >> 18) & 0x07));
-		*out++ = static_cast<uint8_t>(0x80 | ((cp >> 12) & 0x3f));
-		*out++ = static_cast<uint8_t>(0x80 | ((cp >> 6) & 0x3f));
-		*out++ = static_cast<uint8_t>(0x80 | (cp & 0x3f));
+	switch(encoding) {
+	case UTF8:
+		if(cp < 0x80) {
+			*out++ = static_cast<uint8_t>(cp);
+		} else if(cp < 0x0800) {
+			*out++ = static_cast<uint8_t>(0xc0 | ((cp >> 6) & 0x1f));
+			*out++ = static_cast<uint8_t>(0x80 | (cp & 0x3f));
+		} else if(cp < 0x10000) {
+			*out++ = static_cast<uint8_t>(0xe0 | ((cp >> 12) & 0x0f));
+			*out++ = static_cast<uint8_t>(0x80 | ((cp >> 6) & 0x3f));
+			*out++ = static_cast<uint8_t>(0x80 | (cp & 0x3f));
+		} else if(cp < 0x1fffff) {
+			*out++ = static_cast<uint8_t>(0xf0 | ((cp >> 18) & 0x07));
+			*out++ = static_cast<uint8_t>(0x80 | ((cp >> 12) & 0x3f));
+			*out++ = static_cast<uint8_t>(0x80 | ((cp >> 6) & 0x3f));
+			*out++ = static_cast<uint8_t>(0x80 | (cp & 0x3f));
+		}
+		break;
+	default:
+		abort();
 	}
-	
-	return utf8;
+}
+
 }
 
 int main(int argc, char *argv[]) {
-
-	std::vector<uint32_t> codepoints;
 	
-	UtfEncoding inputEncoding = UTF16_LE;
-	
-	
-	
-	std::ifstream file("Event.log", std::ifstream::binary);
+	std::ifstream file("file.txt", std::ifstream::binary);
 	if(file) {
-		switch(inputEncoding) {
-		case UTF16_LE:
-			{
-				uint32_t value;
-				while(readUtf16Value(file, &value)) {
-					codepoints.push_back(value);
-				}
-			}
-			break;
-		default:
-			abort();
-		}
-	}
-	
-	for(uint32_t cp : codepoints) {
-		std::vector<uint8_t> utf8 = codepointToUtf8(cp);
-		for(uint8_t byte : utf8) {
-			std::cout << byte;
+		uint32_t cp;
+		while(utf::read_codepoint(file, utf::UTF16_LE, &cp)) {
+			utf::write_codepoint(cp, utf::UTF8, std::ostream_iterator<uint8_t>(std::cout, ""));
 		}
 	}
 }
